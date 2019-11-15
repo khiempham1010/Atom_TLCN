7e9c5c2f55d8534ef8e7a20ae2c8f696059c40b8
khiempham1010 <pxk.humble@gmail.com>
2019-11-15 14:20:39 +0700

    atom

---
+++ AtomStore/AtomStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtomStore.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtomStore.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            var email = User.GetSpecificClaim("Email");
+            return View();
+        }
+    }
+}
---
+++ AtomStore/AtomStore_Utilities/Constants/CommonConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtomStore.Utilities.Constants
+{
+    public class CommonConstants
+    {
+        
+
+        public const string ProductTag = "Product";
+        public class AppRoles
+        {
+            public const string AdminRole = "Admin";
+        }
+        
+        public class UserClaims
+        {
+            public const string Roles = "Roles"; 
+        }
+    }
+}
